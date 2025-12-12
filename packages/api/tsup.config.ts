c2cc28a25ef395a49ffa3c2d76f38114a3aa9c83
Sagan.ahn <sagan.ahn@kakaocorp.com>
2025-12-12 10:37:42 +0900

    fix(api): DTS 빌드 비활성화
    @mytangerine/core 패키지의 DTS 빌드 문제로 인해
    api 패키지도 일시적으로 DTS 빌드 비활성화

--- packages/api/tsup.config.ts
+++ packages/api/tsup.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'tsup';
 
+/**
+ * TODO: DTS 빌드 재활성화
+ * @mytangerine/core 패키지의 DTS 빌드 문제로 인해 일시적으로 비활성화
+ */
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
-  dts: true,
+  dts: false,
   splitting: false,
   sourcemap: true,
   clean: true,
